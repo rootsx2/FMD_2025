56802e0a96ad0f08ef754fa685117fd6c35e9f82
rootsx2 <61120334+rootsx2@users.noreply.github.com>
2025-03-26 10:45:41 +0200

    Add files via upload

---
+++ styles/adisoutbreaks20250326_093429_1_style.js
@@ -0,0 +1,37 @@
+var size = 0;
+var placement = 'point';
+
+var style_adisoutbreaks20250326_093429_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Reference") !== null) {
+        labelText = String(feature.get("Reference"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 9.533333333333333 + size, points: 5,
+            radius2: 4.766666666666667, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(184,8,8,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 10.4 + size, points: 5,
+            radius2: 5.2, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
